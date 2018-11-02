34d6ada10c7eb8d8f4c71effb166b348689a1c1b
Chris Hassan <chriskkhassan@gmail.com>
2018-11-02 11:56:25 +0000

    Pushing substituting_variables_into_strings_padded_numbers.rb solution

---
+++ 7-kyu-katas/7_substituting_variables_into_strings_padded_numbers.rb
@@ -0,0 +1,9 @@
+# https://www.codewars.com/kata/substituting-variables-into-strings-padded-numbers
+
+def solution(value)
+  "Value is #{"%05d" % value}"
+end
+
+#puts solution(5)
+#puts solution(100)
+#puts solution(10101)
